e65272a62ea947dec886eee234281c8d51456d1f
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-04-13 10:52:20 -0400

    added lightening exercise from chapter

--- script.js
+++ script.js
@@ -119,3 +119,20 @@ console.log(empireStateBuilding[taco])
 console.log(empireStateBuilding[owner])
 console.log(empireStateBuilding[pizza])
 console.log(empireStateBuilding[date])
+
+const nashvilleSoftwareSchool = {
+    founded: 2012,
+    director: "John Wark",
+    instructors: {
+        fullTime: ["Jisie", "Brenda", "Steve", "Joe", "Andy"],
+        partTime: ["Zoe", "Nathan"]
+    },
+    address: "500 Interstate Blvd. S"
+}
+// Lightning Exercise 1: Output the names of the part-time instructors followed by the names of the full-time instructors in the console.
+console.log(nashvilleSoftwareSchool.instructors.partTime, nashvilleSoftwareSchool.instructors.fullTime)
+
+// Lightning Exercise 2: Output only Andy and Zoe in the console.
+console.log(nashvilleSoftwareSchool.instructors.fullTime[4])
+console.log(nashvilleSoftwareSchool.instructors.partTime[0])
+
